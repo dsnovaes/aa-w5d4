4eff324deccc176e56b6f6b6f0101d5ad9ab05e2
Diego Novaes <dsnovaes1@gmail.com>
2022-11-03 15:27:46 -0700

    associations in progress

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,2 +1,40 @@
+# == Schema Information
+#
+# Table name: courses
+#
+#  id            :bigint           not null, primary key
+#  name          :string           not null
+#  prereq_id     :bigint
+#  instructor_id :bigint           not null
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#
 class Course < ApplicationRecord
+    # checks for presence of name
+    validates :name, presence: true,
+
+    # a course can have many enrollments
+    has_many :enrollments,
+        primary_key: :id,
+        foreign_key: :student_id,
+        class_name: :Enrollment
+
+    # a course can have many students
+    has_many :enrolled_students,
+        primary_key: :id,
+        foreign_key: :student_id,
+        class_name: :User
+
+    # a course belong to a instructor
+    belongs_to :instructor,
+        primary_key: :id,
+        foreign_key: :instructor_id,
+        class_name: :User
+
+    # association for prerequisite
+    has_many :prerequisite,
+        through: :course,
+        source: :course,
+        optional: true
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,25 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :bigint           not null, primary key
+#  name       :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class User < ApplicationRecord
+    # checks for presence of name
+    validates :name, presence: true
+
+    has_many :enrollments,
+        primary_key: :id,
+        foreign_key: :student_id,
+        class_name: :Enrollment,
+        dependent: :destroy
+
+    belongs_to :enrolled_courses,
+        primary_key: :id,
+        foreign_key: :course_id,
+        class_name: :Course,
+        dependent: :destroy
 end
